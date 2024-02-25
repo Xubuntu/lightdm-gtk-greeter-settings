7e429dfd544b499514fea284f0ae60c2fd7748ed
Sean Davis <sean@bluesabre.org>
2024-02-24 23:12:34 -0500

    Release 1.2.3

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='lightdm-gtk-greeter-settings',
-    version='1.2.2',
+    version='1.2.3',
     license='GPL-3',
     author='Andrew P.',
     author_email='pan.pav.7c5@gmail.com',
